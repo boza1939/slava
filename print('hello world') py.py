a5289395425af736b4463af798a7c34f0ee5d89d
slava <78681765+boza1939@users.noreply.github.com>
2021-02-07 12:47:05 +0300

    Add files via upload

---
+++ print('hello world') py.py
@@ -0,0 +1,18 @@
+classmates=[]
+apples= 10
+print(apples)
+print('hello world')
+classmates.append('slava')
+classmates.append('svetlana')
+
+
+print(classmates)
+while apples> 0:
+    classmates.append(apples)
+    print(apples)
+    print(classmates)
+    apples=apples -1
+
+
+
+
